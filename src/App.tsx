9e56fdbb416be8f1de539dc053671d517e716edd
ronald-luo <r.luo.180@gmail.com>
2022-10-06 18:12:04 -0400

    add navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
+import Background from './components/background'
+import Navigation from './components/navigation'
 import "./App.css";
 
 function App() {
@@ -7,7 +9,7 @@ function App() {
 
   return (
     <div className="App">
-      <div>
+      {/* <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src="/vite.svg" className="logo" alt="Vite logo" />
         </a>
@@ -26,7 +28,11 @@ function App() {
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
-      </p>
+      </p> */}
+
+
+      {/* <Background/> */}
+      <Navigation/>
     </div>
   );
 }
