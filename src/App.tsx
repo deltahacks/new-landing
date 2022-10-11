fa54f1a0a3d9de6fbade5656cd80c9e1a40622d1
jeffreychan <kkjeffreychan@gmail.com>
2022-10-11 16:46:49 -0400

    editied and merged changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,15 @@
 import { useState } from "react";
 import Navigation from "./components/navigation/navigation";
 import LandingPage from "./views/Landing";
+import clouds from "../public/Clouds.svg";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <div className="App">
+    <div className="landingBG">
+      <img src={clouds} className="absolute h-full w-full" alt="clouds"></img>
       <LandingPage />
-      <Navigation />
     </div>
   );
 }
--- src/components/navigation/navigation.tsx
+++ src/components/navigation/navigation.tsx
@@ -1,35 +1,58 @@
-import React from "react";
 import Logo from "../../assets/Logo.svg";
 import Facebook from "../../assets/Facebook.svg";
 import Instagram from "../../assets/Instagram.svg";
 import Linkedin from "../../assets/Linkedin.svg";
-import TikTok from "../../assets/TikTok.svg";
-import Youtube from "../../assets/Youtube.svg";
-import "./navigation.css";
+import Twitter from "../../assets/Twitter.svg";
+
+const socialIcons = {
+  Facebook: {
+    img: Facebook
+  },
+  Instagram: {
+    img: Instagram
+  },
+  Linkedin: {
+    img: Linkedin
+  },
+  Twitter: {
+    img: Twitter
+  }
+}
+
+const Navbuttons = ["About", "FAQ", "Sponsors", "Contact"]
+
+const SocialIcon = ({ logo, title }: { logo: any, title: string }) => {
+  return (
+    <a href="#" className="mr-2">
+      <img src={logo} alt={title} className="h-8 w-8 stroke-transparent filter fill-white"></img>
+    </a>
+  )
+}
+
+const NavButton = ({ title }: { title: string }) => {
+  return (
+    <a href="#" className="font-minecraft font-white text-white font-light mr-3">
+      {title}
+    </a>
+  )
+}
 
 const Navigation = () => {
   return (
-    <div className="navigation">
-      <a href="#" target="_blank">
-        <img src={Logo} alt="Deltahacks logo" />
-      </a>
+    <div className="absolute flex justify-between items-center bg-slate-600 bg-opacity-40 w-full h-12">
+      <div className="flex justify-center items-center">
+        <a href="#" target="_blank" className="ml-8">
+          <img src={Logo} alt="Deltahacks logo" className="h-10 w-10 mr-4" />
+        </a>
+        {
+          Navbuttons.map((title) => <NavButton key={title} title={title} />)
+        }
+      </div>
 
-      <span className="social-container">
-        <a href="#" target="_blank">
-          <img className="social-logo" src={Facebook} alt="Facebook logo" />
-        </a>
-        <a href="#" target="_blank">
-          <img className="social-logo" src={Instagram} alt="Instagram logo" />
-        </a>
-        <a href="#" target="_blank">
-          <img className="social-logo" src={Linkedin} alt="Linkedin logo" />
-        </a>
-        <a href="#" target="_blank">
-          <img className="social-logo" src={Youtube} alt="Youtube logo" />
-        </a>
-        <a href="#" target="_blank">
-          <img className="social-logo" src={TikTok} alt="TikTok logo" />
-        </a>
+      <span className="flex justify-center align-center mr-8">
+        {
+          Object.entries(socialIcons).map(([key, value]) => <SocialIcon key={key} title={key} logo={value.img} />)
+        }
       </span>
     </div>
   );
