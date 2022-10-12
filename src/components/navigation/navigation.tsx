67a0952b3a214c2d578599a82e0b8c130e23d486
arian81 <ahmadinejadarian@gmail.com>
2022-10-12 02:37:00 -0400

    refactor + add responsiveness

--- src/components/navigation/navigation.tsx
+++ src/components/navigation/navigation.tsx
@@ -1,63 +1,64 @@
-import Logo from "../../../public/Logo_1.svg";
+import Logo from "../../../public/DH9.svg";
 import Facebook from "../../../public/Facebook.svg";
 import Instagram from "../../../public/Instagram.svg";
 import Linkedin from "../../../public/LinkedIn.svg";
 import Twitter from "../../../public/Twitter.svg";
 
 const socialIcons = {
-  Facebook: {
-    img: Facebook,
-  },
-  Instagram: {
-    img: Instagram,
-  },
-  Linkedin: {
-    img: Linkedin,
-  },
-  Twitter: {
-    img: Twitter,
-  },
+    Facebook: {
+        img: Facebook,
+    },
+    Instagram: {
+        img: Instagram,
+    },
+    Linkedin: {
+        img: Linkedin,
+    },
+    Twitter: {
+        img: Twitter,
+    },
 };
 
 const buttons = ["About", "FAQ", "Sponsors", "Contact"];
 
 const SocialIcon = ({ logo, title }: { logo: any; title: string }) => {
-  return (
-    <a href="#" className="mr-2 filter hover:brightness-90">
-      <img src={logo} alt={title} className="h-8 w-8"></img>
-    </a>
-  );
+    return (
+        <a href="#" className="mr-2 filter hover:brightness-90">
+            <img src={logo} alt={title} className="h-8 w-8"></img>
+        </a>
+    );
 };
 
 const NavButton = ({ title }: { title: string }) => {
-  return (
-    <a
-      href="#"
-      className="font-minecraft font-white text-white font-light mr-3"
-    >
-      {title}
-    </a>
-  );
+    return (
+        <a
+            href="#"
+            className="font-minecraft font-white text-white font-light mr-3"
+        >
+            {title}
+        </a>
+    );
 };
 
 const Navigation = () => {
-  return (
-    <div className="flex justify-between items-center bg-slate-600 bg-opacity-40 w-full h-12">
-      <div className="flex justify-center items-center">
-        <a href="#" target="_blank" className="ml-8">
-          <img src={Logo} alt="Deltahacks logo" className="h-10 w-10 mr-4" />
-        </a>
-        {buttons.map((title) => (
-          <NavButton key={title} title={title} />
-        ))}
-      </div>
-      <div className="flex justify-center align-center mr-8">
-        {Object.entries(socialIcons).map(([key, value]) => (
-          <SocialIcon key={key} title={key} logo={value.img} />
-        ))}
-      </div>
-    </div>
-  );
+    return (
+        <div className="flex justify-between items-center bg-slate-600 bg-opacity-40 w-full h-12">
+            <div className="h-9 w-9 flex justify-center align-center ml-2">
+                <img src={Logo} alt="Deltahacks logo" />
+            </div>
+            {/* <div>
+                {buttons.map((title) => (
+                    <NavButton key={title} title={title} />
+                ))}
+            </div> */}
+
+            <div className="flex justify-center align-center">
+                {Object.entries(socialIcons).map(([key, value]) => (
+                    <SocialIcon key={key} title={key} logo={value.img} />
+                ))}
+            </div>
+        </div>
+    );
 };
 
 export default Navigation;
--- src/views/Landing.tsx
+++ src/views/Landing.tsx
@@ -1,57 +1,55 @@
 import logoImage from "../../public/logo.svg";
 import mlh from "../../public/mlh.svg";
 import Navigation from "../components/navigation/navigation";
+import clouds from "../../public/Clouds.png";
 
 const LandingPage = () => {
-  return (
-    <div>
-      <Navigation />
-      <div className="relative flex items-center justify-center flex-col font-minecraft p-14 min-h-screen ">
+    return (
         <div>
-          <img src={logoImage} className="w-full h-full" alt="logo"></img>
-          <div className="flex flex-row sm:pl-24 justify-end items-center mr-36">
-            <img src={mlh} className="w-auto h-4" />
-            <span className="text-sm pl-1 pt-1 whitespace-nowrap">
-              OFFICIAL MEMBER
-            </span>
-          </div>
-        </div>
-        <div className="text-4xl font-bold mb-5"></div>
-        <div className="flex flex-col items-center justify-center font-normal text-lg leading-9 tracking-wider">
-          <p className=" text-center  text-white ">
-            <span className="text-shadow">JANUARY 13-15, 2023</span>
-          </p>
-          <p className=" text-center  text-white">
-            <span className="text-shadow">IN-PERSON</span>
-          </p>
-        </div>
-        <div className="w-100 h-4/5 mx-auto">
-          <div className="flex flex-col items-center  w-100 h-100 mt-10">
-            <div className="mBG px-12 py-2">
-              <a
-                href="#"
-                target="_blank"
-                rel="noreferrer"
-                className="text-white text-center text-2xl tracking-wider"
-              >
-                JOIN OUR MAILING LIST
-              </a>
+            <div className="absolute w-full top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+                <img src={clouds} alt="clouds"></img>
             </div>
-            <div className="mBG px-12 py-2 mt-3">
-              <a
-                href="#"
-                target="_blank"
-                rel="noreferrer"
-                className="text-white text-center text-2xl tracking-wider "
-              >
-                APPLY NOW
-              </a>
+            <Navigation />
+            <div className="relative flex items-center justify-center flex-col font-minecraft p-14 min-h-screen ">
+                <div>
+                    <img
+                        src={logoImage}
+                        className="w-full h-full"
+                        alt="logo"
+                    ></img>
+                    <div className="flex justify-center align-center md:flex-row md:justify-end md:items-center p-1">
+                        <img src={mlh} className="w-auto h-4" />
+                        <span className="text-xs md:text-lg ml-2 whitespace-nowrap">
+                            OFFICIAL MEMBER
+                        </span>
+                    </div>
+                </div>
+                <div className="text-4xl font-bold mb-5"></div>
+                <div className="flex flex-col items-center justify-center font-normal text-lg leading-9 tracking-wider">
+                    <p className=" text-center  text-white ">
+                        <span className="text-shadow">JANUARY 13-15, 2023</span>
+                    </p>
+                    <p className=" text-center  text-white">
+                        <span className="text-shadow">IN-PERSON</span>
+                    </p>
+                </div>
+                <div className="w-100 h-4/5 mx-auto">
+                    <div className="flex flex-col items-center  w-100 h-100 mt-10">
+                        <div className="mBG px-12 py-2 md:px-20 md:py-5 mt-3">
+                            <a
+                                href="#"
+                                target="_blank"
+                                rel="noreferrer"
+                                className="text-white text-center text-xl md:text-3xl tracking-wider "
+                            >
+                                APPLY NOW
+                            </a>
+                        </div>
+                    </div>
+                </div>
             </div>
-          </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default LandingPage;
