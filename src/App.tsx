f04c27d08f0dd1b2f47977f188b52421be9714c4
ronald-luo <67345874+ronald-luo@users.noreply.github.com>
2022-10-12 00:30:06 -0400

    feat: Setup Navbar (#5)
    * add navbar
    
    * fix background
    
    * udpated small changes
    
    * updated small things - fixed conflics
    
    * format
    
    * editied and merged changes
    
    * moved files and small edits
    
    * format
    
    * remove import
    
    * moved images netlify error
    
    * moved imgages
    
    * Fix typo
    
    * fixing font
    
    Co-authored-by: jeffreychan <kkjeffreychan@gmail.com>
    Co-authored-by: Krish <52761222+Krish120003@users.noreply.github.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
 import { useState } from "react";
+import Navigation from "./components/navigation/navigation";
 import LandingPage from "./views/Landing";
+import clouds from "../public/Clouds.svg";
 
 function App() {
+  const [count, setCount] = useState(0);
+
   return (
     <div className="landingBG">
+      <img src={clouds} className="absolute h-full" alt="clouds"></img>
       <LandingPage />
     </div>
   );
--- src/views/Landing.tsx
+++ src/views/Landing.tsx
@@ -1,50 +1,55 @@
 import logoImage from "../../public/logo.svg";
-import clouds from "../../public/Clouds.svg";
+import mlh from "../../public/mlh.svg";
+import Navigation from "../components/navigation/navigation";
 
 const LandingPage = () => {
   return (
-    <div className="flex items-center justify-center flex-col font-minecraft p-14 min-h-screen ">
-      <div>
-        <img src={logoImage} className="px-12" alt="logo"></img>
-        <div className="flex flex-row justify-end items-center mr-36">
-          <img src="/mlh.svg" className="w-auto h-4" />
-          <span className="pl-0.5 pt-0.25">OFFICIAL MEMBER</span>
-        </div>
-      </div>
-      <div className="text-4xl font-bold mb-5"></div>
-      <div className="flex flex-col items-center justify-center font-normal text-lg leading-9 tracking-wider">
-        <p className=" text-center  text-white ">
-          <span className="text-shadow">JANUARY 13-15, 2023</span>
-        </p>
-        <p className=" text-center  text-white">
-          <span className="text-shadow">IN-PERSON</span>
-        </p>
-      </div>
-      <div className="w-100 h-4/5 mx-auto">
-        <div className="flex flex-col items-center  w-100 h-100 mt-10">
-          <div className="mBG px-12 py-2">
-            <a
-              href="#"
-              target="_blank"
-              rel="noreferrer"
-              className="text-white text-center text-2xl tracking-wider"
-            >
-              JOIN OUR MAILING LIST
-            </a>
-          </div>
-          <div className="mBG px-12 py-2 mt-3">
-            <a
-              href="#"
-              target="_blank"
-              rel="noreferrer"
-              className="text-white text-center text-2xl tracking-wider "
-            >
-              APPLY NOW
-            </a>
+    <div>
+      <Navigation />
+      <div className="relative flex items-center justify-center flex-col font-minecraft p-14 min-h-screen ">
+        <div>
+          <img src={logoImage} className="w-full h-full" alt="logo"></img>
+          <div className="flex flex-row sm:pl-24 justify-end items-center mr-36">
+            <img src={mlh} className="w-auto h-4" />
+            <span className="text-sm pl-1 pt-1 whitespace-nowrap">
+              OFFICIAL MEMBER
+            </span>
           </div>
         </div>
+        <div className="text-4xl font-bold mb-5"></div>
+        <div className="flex flex-col items-center justify-center font-normal text-lg leading-9 tracking-wider">
+          <p className=" text-center  text-white ">
+            <span className="text-shadow">JANUARY 13-15, 2023</span>
+          </p>
+          <p className=" text-center  text-white">
+            <span className="text-shadow">IN-PERSON</span>
+          </p>
+        </div>
+        <div className="w-100 h-4/5 mx-auto">
+          <div className="flex flex-col items-center  w-100 h-100 mt-10">
+            <div className="mBG px-12 py-2">
+              <a
+                href="#"
+                target="_blank"
+                rel="noreferrer"
+                className="text-white text-center text-2xl tracking-wider"
+              >
+                JOIN OUR MAILING LIST
+              </a>
+            </div>
+            <div className="mBG px-12 py-2 mt-3">
+              <a
+                href="#"
+                target="_blank"
+                rel="noreferrer"
+                className="text-white text-center text-2xl tracking-wider "
+              >
+                APPLY NOW
+              </a>
+            </div>
+          </div>
+        </div>
       </div>
-      <img src={clouds} className="absolute h-full w-full" alt="clouds"></img>
     </div>
   );
 };
