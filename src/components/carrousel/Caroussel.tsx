bb7ff297baa09dda222811de7786835748fd73d4
fvcci <fongf2@mcmaster.ca>
2022-11-12 18:43:53 -0500

    formatted stuff

--- src/components/carrousel/Caroussel.tsx
+++ src/components/carrousel/Caroussel.tsx
@@ -1,5 +1,5 @@
-import {useState, useEffect} from "react";
-import CarousselPane, {mod} from "./CarousselPane";
+import { useState, useEffect } from "react";
+import CarousselPane, { mod } from "./CarousselPane";
 
 // ! not adaptive enough for IPHONE SE or samsung galaxy s8+
 const Carrousel = () => {
@@ -8,7 +8,8 @@ const Carrousel = () => {
 
   // TODO put title and descriptions in an object and iterate through that
   const carroussel = [
-    { title: "About Us",
+    {
+      title: "About Us",
       desktopDescription: [
         "At DeltaHacks, we believe change comes from dreaming big.",
         "Each year we enable over 800 students from across North America, working hard over 36 hours, to bring their big ideas to life.",
@@ -21,9 +22,10 @@ const Carrousel = () => {
         "Each year we enable 800+ students across North America to bring their big ideas to life.",
         "Our team works year round to give the platform to make an idea a reality.",
         "Unleash your creativity, at DeltaHacks 9!",
-      ]
+      ],
     },
-    { title: "The Hackathon for Change",
+    {
+      title: "The Hackathon for Change",
       desktopDescription: [
         "DeltaHacks is the annual hackathon for change, it's even in the name!",
         "The primary focus of DeltaHacks is to enable hackers from all over to make a positive change in any field! We work with McMaster University to welcome hackers of all levels!",
@@ -34,9 +36,10 @@ const Carrousel = () => {
         "DeltaHacks is the annual hackathon for change.",
         "The primary focus of DeltaHacks is to enable hackers from all over to make a positive change in any field! We work with McMaster University to welcome hackers of all levels!",
         // "Now in our 9th year running, DeltaHacks 9 is our biggest and best yet. Join us this January for an experience you won’t forget!",
-      ]
+      ],
     },
-    { title: "Past Hacks",
+    {
+      title: "Past Hacks",
       desktopDescription: [
         "Environmental Hacks works towards a more sustainable future, from reducing emissions and overall global warming potential to helping individuals make more conscious and environmentally friendly choices!",
         "The Education Hack aims to empower students, professors, and other people in academia to create technological solutions to augment learning.",
@@ -45,9 +48,10 @@ const Carrousel = () => {
       mobileDescription: [
         "Environmental Hacks works towards a sustainable future from educating individuals to globally reducing emissions.",
         "The Education Hack empowers people in academia to create technological solutions for learning.",
-      ]
+      ],
     },
-    { title: "More Hacks!",
+    {
+      title: "More Hacks!",
       desktopDescription: [
         "Our Health Hack has helped students think about impacting other industries that could use the solutions they create to save lives! With the many changes to modern medicine happening everyday, it only makes sense that the integration of technology in modern medicine is vital to enabling the industry to grow.",
         "The Productivity Hack is a hack we conceived to boost the work you could get done in a day, or perhaps just create something that could make a lot of people's lives significantly easier!",
@@ -55,56 +59,71 @@ const Carrousel = () => {
       mobileDescription: [
         "Our Health Hack has helped students think about impacting other industries to save lives! The integration of technology into modern medecine is vital the growth of the industry.",
         "The Productivity Hack is about boosting daily productivity, or making people's lives significantly easier!",
-      ]
+      ],
     },
-  ]
+  ];
 
   useEffect(() => {
     setTimeout(() => {
       setAnimateArrows(-animateArrows);
     }, 1000);
-  }, [animateArrows])
+  }, [animateArrows]);
 
   return (
     <div className="px-8 sm:px-14 overflow-hidden">
       <div className="min-h-screen w-full relative flex items-center justify-center flex-col">
-        {carroussel.map((pane, id) =>
-          <CarousselPane key={id} id={id} numPanes={carroussel.length} activePane={activePane}>
+        {carroussel.map((pane, id) => (
+          <CarousselPane
+            key={id}
+            id={id}
+            numPanes={carroussel.length}
+            activePane={activePane}
+          >
             <div className="hidden sm:block space-y-4">
               <h1 className="text-4xl">{pane.title}</h1>
               <br />
-              {pane.desktopDescription.map((line, i) => <p key={i}>{line}</p>)}
+              {pane.desktopDescription.map((line, i) => (
+                <p key={i}>{line}</p>
+              ))}
             </div>
             <div className="sm:hidden space-y-4 text-sm">
               <h1 className="text-3xl">{pane.title}</h1>
-              {pane.mobileDescription.map((line, i) => <p key={i}>{line}</p>)}
+              {pane.mobileDescription.map((line, i) => (
+                <p key={i}>{line}</p>
+              ))}
             </div>
           </CarousselPane>
-        )}
+        ))}
         <div className="absolute t-0 r-0 b-0 l-0 m-auto h-4/6 sm:h-3/5 w-full max-w-4xl">
           <div className="z-40 relative h-full w-full">
             <button
               className="text-6xl font-bold text-gray-200 duration-1000"
               style={{
-                textShadow: "-3px -3px black, -3px 3px black, 3px 3px black, 3px -3px black",
+                textShadow:
+                  "-3px -3px black, -3px 3px black, 3px 3px black, 3px -3px black",
                 position: "absolute",
-                top: '50%',
+                top: "50%",
                 transform: `translate(${-animateArrows}px, -50%)`,
               }}
-              onClick={() => setActivePane(mod(activePane-1, carroussel.length))}
+              onClick={() =>
+                setActivePane(mod(activePane - 1, carroussel.length))
+              }
             >
               {"<"}
             </button>
             <button
               className="text-6xl font-bold text-gray-200 duration-1000"
               style={{
-                textShadow: "-3px -3px black, -3px 3px black, 3px 3px black, 3px -3px black",
+                textShadow:
+                  "-3px -3px black, -3px 3px black, 3px 3px black, 3px -3px black",
                 position: "absolute",
-                top: '50%',
+                top: "50%",
                 right: "0",
                 transform: `translate(${animateArrows}px, -50%)`,
               }}
-              onClick={() => setActivePane(mod(activePane+1, carroussel.length))}
+              onClick={() =>
+                setActivePane(mod(activePane + 1, carroussel.length))
+              }
             >
               {">"}
             </button>
@@ -112,7 +131,7 @@ const Carrousel = () => {
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default Carrousel;
--- src/components/carrousel/CarousselPane.tsx
+++ src/components/carrousel/CarousselPane.tsx
@@ -3,46 +3,52 @@ interface Props {
   id: number;
   numPanes: number;
   activePane: number;
-};
+}
 
-export const mod = (k:number, n: number): number => {
-  let res = k%n;
-  if (res<0) {
-    return res+n;
+export const mod = (k: number, n: number): number => {
+  let res = k % n;
+  if (res < 0) {
+    return res + n;
   } else {
     return res;
   }
-}
+};
 
-const CarousselPane = ({children, id, numPanes, activePane}: Props) => {
+const CarousselPane = ({ children, id, numPanes, activePane }: Props) => {
   let style;
   // Active pane
-  if (id==activePane) {
-    style = "opacity-100 scale-100 z-20"
+  if (id == activePane) {
+    style = "opacity-100 scale-100 z-20";
 
-  // Left inactive
-  } else if (id==mod(activePane-1, numPanes)) {
-    style = "translate-x-3/4 opacity-90 scale-75 duration-500 z-10"
+    // Left inactive
+  } else if (id == mod(activePane - 1, numPanes)) {
+    style = "translate-x-3/4 opacity-90 scale-75 duration-500 z-10";
 
-  // Right inactive
-  } else if (id==mod(activePane+1, numPanes)) {
-    style = "-translate-x-3/4 opacity-90 scale-75 duration-500 z-10"
+    // Right inactive
+  } else if (id == mod(activePane + 1, numPanes)) {
+    style = "-translate-x-3/4 opacity-90 scale-75 duration-500 z-10";
 
-  // default
+    // default
   } else {
-    style = "opacity-100 opacity-30 scale-50 z-0"
+    style = "opacity-100 opacity-30 scale-50 z-0";
   }
 
-  return (<>
-    <div className={`absolute t-0 r-0 b-0 l-0 m-auto h-fit lg:h-3/5 w-full max-w-3xl p-3
+  return (
+    <>
+      <div
+        className={`absolute t-0 r-0 b-0 l-0 m-auto h-fit lg:h-3/5 w-full max-w-3xl p-3
       bg-gradient-to-b from-white to-zinc-400
-      duration-500 ${style}`}>
-      <div className="h-fit lg:h-full w-full text-white p-8 sm:p-11
-        bg-gradient-to-b from-neutral-600 to-neutral-800">
-        {children}
+      duration-500 ${style}`}
+      >
+        <div
+          className="h-fit lg:h-full w-full text-white p-8 sm:p-11
+        bg-gradient-to-b from-neutral-600 to-neutral-800"
+        >
+          {children}
+        </div>
       </div>
-    </div>
-  </>);
-}
+    </>
+  );
+};
 
 export default CarousselPane;
