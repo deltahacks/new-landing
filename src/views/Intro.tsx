5523bb86c66a088c246edebdb686362c9083eb39
fvcci <fongf2@mcmaster.ca>
2022-11-15 13:06:08 -0500

    fixed cloud stuff

--- src/views/Intro.tsx
+++ src/views/Intro.tsx
@@ -5,12 +5,12 @@ import clouds from "../../public/Clouds.png";
 const Intro = () => {
   return (
     <>
-      <div className="relative flex items-center justify-center flex-col font-minecraft p-14 min-h-screen ">
+      <div className="relative flex items-center justify-center flex-col font-minecraft px-14 pt-32 min-h-fit sm:min-h-screen">
         <div className="absolute w-full top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
-          <img src={clouds} alt="clouds"></img>
+          <img src={clouds} alt="clouds" />
         </div>
-        <div>
-          <img src={logoImage} className="w-full h-full" alt="logo"></img>
+        <div className="z-10">
+          <img src={logoImage} className="w-full h-full z-10" alt="logo" />
           <div className="flex justify-center align-center md:flex-row md:justify-end md:items-center p-1">
             <img src={mlh} className="w-auto h-4" />
             <span className="text-xs md:text-lg ml-2 whitespace-nowrap z-10">
@@ -19,7 +19,7 @@ const Intro = () => {
           </div>
         </div>
         <div className="text-4xl font-bold mb-5"></div>
-        <div className="flex flex-col items-center justify-center font-normal text-lg leading-9 tracking-wider">
+        <div className="flex flex-col items-center justify-center font-normal text-lg leading-9 tracking-wider z-10git ">
           <p className=" text-center  text-white ">
             <span className="text-shadow">JANUARY 13-15, 2023</span>
           </p>
