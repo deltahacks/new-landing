34712d571c6103e99f75b434a6e5f4b6ee698ac5
fvcci <fongf2@mcmaster.ca>
2022-11-15 17:40:44 -0500

    formatted everything with prettier

--- src/views/Landing.tsx
+++ src/views/Landing.tsx
@@ -11,43 +11,55 @@ import greenIsland from "../../public/green-island.png";
 import greenIslandBlurred from "../../public/green-island-blurred.png";
 
 const LandingPage = () => {
-  const Island = ({ img, dimensions, position }: {
-      img: string, dimensions: string, position: { left?: number, right?: number }
-    }) => <>
-    <img
-      className={`absolute ${dimensions} pointer-events-none object-cover duration-1000`}
-      style={position}
-      src={img}
-      alt={img.substring(img.lastIndexOf("/") + 1)}
-    />
-  </>;
+  const Island = ({
+    img,
+    dimensions,
+    position,
+  }: {
+    img: string;
+    dimensions: string;
+    position: { left?: number; right?: number };
+  }) => (
+    <>
+      <img
+        className={`absolute ${dimensions} pointer-events-none object-cover duration-1000`}
+        style={position}
+        src={img}
+        alt={img.substring(img.lastIndexOf("/") + 1)}
+      />
+    </>
+  );
 
   const Islands = () => (
     <>
       <Island
         img={snowyIslandBlurriest}
         dimensions={"top-[55vh] sm:top-[90vh] lg:top-[120vh] z-20 w-1/4"}
-        position={{left: 0}}
+        position={{ left: 0 }}
       />
       <Island
         img={snowyIslandBlurred}
         dimensions={"top-[65vh] sm:top-[95vh] z-0 w-3/5 lg:w-2/5"}
-        position={{right: 0}}
+        position={{ right: 0 }}
       />
       <Island
         img={snowyIsland}
         dimensions={"top-[110vh] sm:top-[170vh] z-0 lg:z-20 w-2/3 lg:w-1/2"}
-        position={{right: 0}}
+        position={{ right: 0 }}
       />
       <Island
         img={greenIsland}
-        dimensions={"top-[150vh] sm:top-[200vh] lg:top-[220vh] z-0 lg:z-30 w-2/3 lg:w-1/2"}
-        position={{left: 0}}
+        dimensions={
+          "top-[150vh] sm:top-[200vh] lg:top-[220vh] z-0 lg:z-30 w-2/3 lg:w-1/2"
+        }
+        position={{ left: 0 }}
       />
       <Island
         img={greenIslandBlurred}
-        dimensions={"top-[170vh] sm:top-[230vh] lg:top-[220vh] z-30 lg:z-30 w-1/4 lg:w-1/4"}
-        position={{right: 0}}
+        dimensions={
+          "top-[170vh] sm:top-[230vh] lg:top-[220vh] z-30 lg:z-30 w-1/4 lg:w-1/4"
+        }
+        position={{ right: 0 }}
       />
     </>
   );
