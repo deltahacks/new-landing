f5f9cd45749bc773dfd1e15f79ab04763f38f4fd
fvcci <fongf2@mcmaster.ca>
2022-11-15 17:31:31 -0500

    repositioned islands for better mobile experience

--- src/views/Landing.tsx
+++ src/views/Landing.tsx
@@ -11,16 +11,31 @@ import greenIsland from "../../public/green-island.png";
 import greenIslandBlurred from "../../public/green-island-blurred.png";
 
 const LandingPage = () => {
+  const Island = ({ img, dimensions, position }: {
+      img: string, dimensions: string, position: { left?: number, right?: number }
+    }) => <>
+    <img
+      className={`absolute ${dimensions} object-cover duration-1000`}
+      style={position}
+      src={img}
+      alt={img.substring(img.lastIndexOf("/") + 1)}
+    />
+  </>;
+
   const Islands = () => (
     <>
-      <img
-        className="absolute top-[55vh] sm:top-[90vh] lg:top-[120vh] z-20 w-1/4 object-cover duration-1000"
-        style={{ left: 0 }}
-        src={snowyIslandBlurriest}
-        alt="snowy-island-blurriest.png"
+      <Island
+        img={snowyIslandBlurriest}
+        dimensions={"top-[55vh] sm:top-[90vh] lg:top-[120vh] z-20 w-1/4"}
+        position={{left: 0}}
+      />
+      <Island
+        img={snowyIslandBlurred}
+        dimensions={"top-[65vh] sm:top-[95vh] z-0 w-3/5 lg:w-2/5"}
+        position={{right: 0}}
       />
       <img
-        className="absolute top-[65vh] sm:top-[95vh] z-0 w-3/5 lg:w-2/5 object-cover duration-1000"
+        className="absolute  object-cover duration-1000"
         style={{ right: 0 }}
         src={snowyIslandBlurred}
         alt="snowy-island-blurred.png"
@@ -32,13 +47,13 @@ const LandingPage = () => {
         alt="snowy-island.png"
       />
       <img
-        className="absolute top-[200vh] lg:top-[230vh] z-0 lg:z-30 w-2/3 lg:w-1/2 object-cover duration-1000"
+        className="absolute top-[150vh] sm:top-[200vh] lg:top-[230vh] z-0 lg:z-30 w-2/3 lg:w-1/2 object-cover duration-1000"
         style={{ left: 0 }}
         src={greenIsland}
         alt="green-island.png"
       />
       <img
-        className="absolute top-[220vh] lg:top-[230vh] z-0 lg:z-30 w-1/4 lg:w-1/4 object-cover duration-1000"
+        className="absolute top-[170vh] sm:top-[220vh] lg:top-[230vh] z-30 lg:z-30 w-1/4 lg:w-1/4 object-cover pointer-events-none duration-1000"
         style={{ right: 0 }}
         src={greenIslandBlurred}
         alt="green-island.png"
