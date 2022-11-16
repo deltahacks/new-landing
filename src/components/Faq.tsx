2d09e23daefc27e6d365e39156ac12c2e27b5140
Rachelle DeMan <demanr@mcmaster.ca>
2022-11-16 02:25:48 -0500

    fix: added section header/indicator

--- src/components/Faq.tsx
+++ src/components/Faq.tsx
@@ -82,7 +82,15 @@ const categories = {
 };
 const Faq = () => {
   return (
-    <div className="lg:w-full py-10">
+    <div className="lg:w-full sm:px-48 py-10">
+      <div className="p-12">
+        <div className="shadow-none outline-none transform px-12 py-2 md:px-10 md:py-4 text-center focus:bg-transparent text-xl md:text-3xl">
+          Frequently Asked Questions
+        </div>
+        <div className="text-center text-2xl md:text-4xl align-middle rotate-90 transform">
+          {">"}
+        </div>
+      </div>
       <Tab.Group defaultIndex={0}>
         <div className="gap-x-1">
           <div className="flex flex-wrap lg:flex-nowrap justify-center w-full align-center font-minecraft text-white md:text-xl gap-5">
