fda0aa3985895906736c171e2e892a2301a2a577
Rachelle DeMan <demanr@mcmaster.ca>
2022-11-15 19:40:47 -0500

    fix: removed unused code

--- src/components/Sponsors.tsx
+++ src/components/Sponsors.tsx
@@ -29,83 +29,21 @@ const Sponsors = () => {
       >
         <div className="rotate-90 transform">{"<"}</div>
       </div>
-      { handleShow ?
+      {handleShow ? (
         <div className="min-h-screen">
-            <div className="w-1/5 absolute left-1/2 transform -translate-x-1/2">
-                <img src={RBC} className=""alt="RBC logo"></img>
-            </div>
-            <div className="w-1/3 absolute top-1/2 md:top-[60%] lg:top-[70%] right-1/2 transform -translate-x-1/3">
-                <img src={Arcelor} className=""alt="ArcelorMittal logo"></img>
-            </div>
-            <div className="w-1/3 absolute top-1/2 md:top-[60%] lg:top-[70%] left-1/2 transform translate-x-1/3">
-                <img src={Uizard} className="self-center"alt="Uizard logo"></img>
+          <div className="w-1/5 absolute left-1/2 transform -translate-x-1/2">
+            <img src={RBC} className="" alt="RBC logo"></img>
           </div>
-      </div> : null}
+          <div className="w-1/3 absolute top-1/2 md:top-[60%] lg:top-[70%] right-1/2 transform -translate-x-1/3">
+            <img src={Arcelor} className="" alt="ArcelorMittal logo"></img>
+          </div>
+          <div className="w-1/3 absolute top-1/2 md:top-[60%] lg:top-[70%] left-1/2 transform translate-x-1/3">
+            <img src={Uizard} className="self-center" alt="Uizard logo"></img>
+          </div>
+        </div>
+      ) : null}
     </div>
   );
 };
 
 export default Sponsors;
-/*
-<Disclosure>
-<Disclosure.Button
-  onPointerUp={() => {
-    setOpen(!open);
-  }}
-  className="px-12 py-2 md:px-10 md:py-3 text-center focus:outline-none text-xl md:text-2xl"
->
-  Our Sponsors
-</Disclosure.Button>
-<div
-  className={classNames(
-    "text-center text-4xl align-middle transform",
-    open ? "rotate-180 transform md:py-0 " : ""
-  )}
->
-  <div className="rotate-90 transform">{">"}</div>
-</div>
-<Disclosure.Panel className="">
-  <div className="absolute -translate-x-[30%] ">
-    <img src={RBC} alt="RBC logo"></img>
-  </div>
-  <div className="absolute">
-    <img src={Uizard} alt="Uizard logo"></img>
-  </div>
-  <div className="absolute">
-    <img src={Arcelor} alt="ArcelorMittal logo"></img>
-  </div>
-</Disclosure.Panel>
-
-</Disclosure>
-
-
-<button
-        onClick={() => {
-          setHandleShow(!handleShow);
-        }}
-        className="px-12 py-2 md:px-10 md:py-3 text-center focus:outline-none text-xl md:text-2xl"
-      >
-        Our sponsors
-      </button>
-      <div
-        className={classNames(
-          "text-center text-4xl align-middle transform",
-          handleShow ? "rotate-180 transform md:py-0 " : ""
-        )}
-      >
-        <div className="rotate-90 transform">{"<"}</div>
-      </div>
-      { handleShow ?
-      <div className="absolute w-full">
-        <div className="">
-            <img src={RBC} alt="RBC logo"></img>
-        </div>
-        <div className="">
-            <img src={Uizard} alt="Uizard logo"></img>
-        </div>
-        <div className="">
-            <img src={Arcelor} alt="ArcelorMittal logo"></img>
-        </div>
-      </div> : null}
-        
-*/
