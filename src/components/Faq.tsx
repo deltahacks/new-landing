1f39be191772ac9d3a298b2957192aeca960240d
fvcci <fongf2@mcmaster.ca>
2022-11-17 13:58:43 -0500

    fixed errors

--- src/components/Faq.tsx
+++ src/components/Faq.tsx
@@ -101,9 +101,9 @@ const Faq = () => {
 
   return (
     <div className="lg:w-full sm:px-48 py-10 z-50">
-      <div className="p-12 pt-0 md:pl-80 text-white">
-        <div className=" shadow-none outline-none transform px-12 py-2 md:px-10 md:py-4 text-center focus:bg-transparent text-xl md:text-3xl">
-          Frequently Asked<div>Questions</div>
+      <div className="p-12 pt-0 text-white">
+        <div className="shadow-none outline-none transform px-12 py-2 md:px-10 md:py-4 text-center focus:bg-transparent text-xl md:text-3xl">
+          Frequently Asked Questions
         </div>
         <div className="text-center text-2xl md:text-4xl align-middle rotate-90 transform">
           {">"}
