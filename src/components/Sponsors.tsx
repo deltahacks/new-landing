74f1551d326ee3dfc7c4fca3eee68814272fe784
Rachelle DeMan <demanr@mcmaster.ca>
2022-12-24 01:27:13 -0500

    chore: added more sponsors

--- src/components/Sponsors.tsx
+++ src/components/Sponsors.tsx
@@ -1,6 +1,12 @@
 import RBC from "../../public/RBC.png";
-import Uizard from "../../public/Uizard.png";
 import Arcelor from "../../public/ArcelorMittal.png";
+import Balsamiq from "../../public/Balsamiq-logo-alt.png";
+import FDM from "../../public/FDM.svg";
+import Fireball from "../../public/fireball_eng.png";
+import MacMatSci from "../../public/mcm-matsci_left-rev_png.png";
+import Siemens from "../../public/fireball_eng.png";
+import Verbwire from "../../public/Verbwire.svg";
+import Stickeryou from "../../public/SY_logo.png";
 
 const Sponsors = () => {
   return (
@@ -15,11 +21,39 @@ const Sponsors = () => {
       </div>
       <div className="w-full relative flex justify-center items-center lg:px-44 lg:pb-32 flex-wrap flex-col">
         <div className="w-1/5 lg:w-1/2 flex items-center justify-center">
-          <img src={RBC} className="" alt="RBC logo"></img>
+          <a href="https://www.rbc.com/canada.html">
+            <img src={RBC} className="" alt="RBC logo"></img>
+          </a>
         </div>
-        <div className="flex justify-evenly items-center lg:gap-64 py-8">
-          <img src={Arcelor} className="w-2/5" alt="ArcelorMittal logo"></img>
-          <img src={Uizard} className="w-2/5" alt="Uizard logo"></img>
+        <div className="flex justify-evenly items-center lg:gap-32 py-8">
+          <a className="w-2/5" href="https://corporate.arcelormittal.com/">
+            <img src={Arcelor} alt="ArcelorMittal logo"></img>
+          </a>
+          <a className="w-2/5" href="https://balsamiq.com/">
+            <img src={Balsamiq} alt="Balsamiq logo"></img>
+          </a>
+        </div>
+      </div>
+      <div className="w-full relative flex justify-center items-center lg:px-44 lg:pb-32 flex-wrap flex-col">
+        <div className="w-1/5 lg:w-1/2 flex items-center justify-center">
+          <a href="https://www.fdmgroup.com/en-us/home-us/">
+            <img src={FDM} className="" alt="FDM logo"></img>
+          </a>
+        </div>
+        <div className="flex justify-evenly items-center lg:gap-32 py-8">
+          <a className="w-2/5" href="https://www.eng.mcmaster.ca/">
+            <img src={Fireball} alt="McMaster Fireball Logo"></img>
+          </a>
+          <a className="w-2/5" href="https://www.eng.mcmaster.ca/materials">
+            <img src={MacMatSci} alt="McMaster Material Science Logo"></img>
+          </a>
+        </div>
+      </div>
+      <div className="w-full relative flex justify-center items-center lg:px-44 lg:pb-32 flex-wrap flex-col">
+        <div className="w-1/5 lg:w-1/2 flex items-center justify-center">
+          <a href="https://www.siemens.com/global/en.html">
+            <img src={Siemens} className="" alt="Siemens Logo"></img>
+          </a>
         </div>
       </div>
     </div>
